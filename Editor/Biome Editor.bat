0bfa1364c01588788a5c74616b76d7679a5ac5a8
gone2quick <85038302+gone2quick@users.noreply.github.com>
2025-10-14 12:49:26 -0400

    Update Biome Editor.bat

--- Editor/Biome Editor.bat
+++ Editor/Biome Editor.bat
@@ -1,3 +1,22 @@
 @echo off
 cd /d "%~dp0"
-python map_editor.py
+start "" /min python map_editor.py
+timeout /t 1 >nul
+powershell -command "
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+public class Win {
+    [DllImport(\"user32.dll\")] public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    [DllImport(\"user32.dll\")] public static extern IntPtr FindWindowEx(IntPtr parent, IntPtr child, string className, string windowTitle);
+    [DllImport(\"user32.dll\")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport(\"user32.dll\")] public static extern bool SetForegroundWindow(IntPtr hWnd);
+}
+'@;
+$hwnd = [Win]::FindWindow('TkTopLevel', $null)
+if ($hwnd -eq [IntPtr]::Zero) { $hwnd = [Win]::FindWindow('Python', $null) }
+if ($hwnd -ne [IntPtr]::Zero) {
+    [Win]::ShowWindow($hwnd, 3)
+    [Win]::SetForegroundWindow($hwnd)
+}
+"
